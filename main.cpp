1ad025515dcc0aeb37de1c4e9d71f66e2956a5c8
rsethc <rsethc@gmail.com>
2015-03-23 00:56:54 -0400

    on-Quit code (Save/Discard/Cancel dialog)
    with comments

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ struct cbVec2
 {
 	cbScalar x,y;
 };
+SDL_Renderer* renderer;
 #include <menu.h>
 struct cbLD_Circle
 {
@@ -126,12 +127,110 @@ void cbRenderComponents ()
 #include <stdio.h>
 #include <SDL_image.h>
 #define SampleImagePath "ticktock.bmp"
+void SaveToPath (char* path)
+{
+	FILE* file = fopen(path,"w");
+	if (file)
+	{
+		fprintf(file,"#yoloswag");
+		fclose(file);
+	}
+	else printf("Could not open file to save project.\n");
+	// Save the current project to the file specified by [path].
+};
+void Save ()
+{
+	// Bring up a dialog for where to save the project. Then call SaveToPath( with the path ).
+	SaveToPath("project.cbpf"); // temp path
+	// ".cbpf" extension should be automatically added. "Circuit Builder Project File"
+	// not ".cbp" because that would interfere with Code::Blocks project files and get annoying to us developers
+
+	// of course, warn the user before trying to save over an existing file path!
+};
+SDL_Window* window;
+bool AttemptQuit ()
+{
+	// This returns [true] to signal that the app should exit.
+	const SDL_MessageBoxButtonData buttons [] =
+	{
+		// Ordering is funky due to the way SDL sorts by array position, not by [.buttonid].
+		/* {.flags,.buttonid,.text} */
+		{SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT,	2,	"Cancel"	},
+		{0,											0,	"Discard"	},
+		{SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT,	1,	"Save"		},
+	};
+	const SDL_MessageBoxColorScheme colorScheme =
+	{
+		/* .colors */
+		{
+			/* {.r,.g,.b} */
+			/* SDL_MESSAGEBOX_COLOR_BACKGROUND */
+			{223,223,223},
+			/* SDL_MESSAGEBOX_COLOR_TEXT */
+			{255,0,  0  },
+			/* SDL_MESSAGEBOX_COLOR_BUTTON_BORDER */
+			{0,  0,  0  },
+			/* SDL_MESSAGEBOX_COLOR_BUTTON_BACKGROUND */
+			{191,191,191},
+			/* SDL_MESSAGEBOX_COLOR_BUTTON_SELECTED */
+			{255,255,255}
+		}
+	};
+	const SDL_MessageBoxData messageboxdata = {
+		SDL_MESSAGEBOX_WARNING, /* .flags */
+		window, /* .window */
+		"Unsaved Changes", /* .title */
+		"If you quit without saving, your unsaved work will be lost!", /* .message */
+		SDL_arraysize(buttons), /* .numbuttons */
+		buttons, /* .buttons */
+		&colorScheme /* .colorScheme */
+	};
+	int buttonid;
+	if (SDL_ShowMessageBox(&messageboxdata, &buttonid) < 0) printf("Error displaying message box.\n");
+	else // If the message box can't be displayed, act like CANCEL was selected.
+	{
+		printf("Button Result: %d\n",buttonid);
+		if (buttonid != 2) // #2: CANCEL
+		{
+			if (buttonid) Save(); // Not #0, so #1: SAVE
+			return true;
+		};
+	};
+	return false;
+};
+bool AltKeyDown_Left = false;
+bool AltKeyDown_Right = false;
+bool CheckInput ()
+{
+	// This returns [true] to continue the app's main loop, or [false] to kill it and cause an exit.
+	SDL_Event event;
+	while (SDL_PollEvent(&event))
+	{
+		if (event.type == SDL_QUIT) if (AttemptQuit()) return false;
+		else if (event.type == SDL_KEYDOWN)
+		{
+			if (event.key.keysym.sym == SDLK_LALT) AltKeyDown_Left = true;
+			else if (event.key.keysym.sym == SDLK_RALT) AltKeyDown_Right = true;
+			else if (event.key.keysym.sym == SDLK_F4) if (AttemptQuit()) return false; // ALT+F4
+			else if (AltKeyDown_Left || AltKeyDown_Right)
+			{
+				// Check for DropDownTab/DropDownOption hotkeys, since Alt is being held.
+			};
+		}
+		else if (event.type == SDL_KEYUP)
+		{
+			if (event.key.keysym.sym == SDLK_LALT) AltKeyDown_Left = false;
+			else if (event.key.keysym.sym == SDLK_RALT) AltKeyDown_Right = false;
+		};
+	};
+	return true;
+};
 int main ()
 {
     if (SDL_Init(SDL_INIT_EVERYTHING)) printf("Could not initialize SDL.\n");
-    SDL_Window* window = SDL_CreateWindow("Circuit Builder",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,1280,720,SDL_WINDOW_RESIZABLE);
+    window = SDL_CreateWindow("Circuit Builder",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,1280,720,SDL_WINDOW_RESIZABLE);
     if (!window) printf("Could not initialize window.\n");
-    SDL_Renderer* renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_PRESENTVSYNC);
+    renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_PRESENTVSYNC);
     if (!renderer) printf("Could not initialize renderer.\n");
     SDL_Surface* ticktock = IMG_Load(SampleImagePath);
     if (!ticktock) printf("Could not load sample image (" SampleImagePath ").\n");
@@ -141,13 +240,9 @@ int main ()
     int image_h = ticktock->h;
 	cbScalar imageaspect = image_w / (cbScalar)image_h;
 	SDL_FreeSurface(ticktock);
-	cbIndex frame_id = 0;
 	SDL_SetRenderDrawColor(renderer,255,255,255,255);
-    while (true)
+    while (CheckInput())
 	{
-		SDL_Event event;
-		while (SDL_PollEvent(&event)) if (event.type == SDL_QUIT) goto QUIT;
-		frame_id++;
 		SDL_RenderFillRect(renderer,NULL);
 		int screen_w;
 		int screen_h;
--- menu.h
+++ menu.h
@@ -1,7 +1,36 @@
 #pragma once
-struct MenuBox
+/// Everything is 100% opacity unless we want to do UI sorting to prevent artifacts. No? Didn't think so.
+// down the road, the UI should be customizable in an options menu
+// maybe even draggable on the fly
+// but first let's finish the rest of the app
+Uint8 DropDownTabColor_R = 191;
+Uint8 DropDownTabColor_G = 159;
+Uint8 DropDownTabColor_B = 127;
+Uint8 DropDownItemColor_R = 159;
+Uint8 DropDownItemColor_G = 159;
+Uint8 DropDownItemColor_B = 159;
+cbScalar DropDownHeight = 0.02; // height of bar and items
+struct DropDownOption // Things under DropDownTabs, such as File->SAVE or Edit->UNDO
 {
-	cbVec2 topleft,dims;
 	char* text;
-	char* logopath;
+	Uint32 hotkey;
+	SDL_Texture* icon; // icon only drawn if non-NULL. (not required)
+};
+struct DropDownTab // tabs at the top of screen like FILE or EDIT
+{
+	char* text;
+	Uint32 hotkey; // used in Alt+[hotkey] as alternative to clicking
+	DropDownOption* options; // only drawn if this tab is active
+	cbIndex optioncount; // number of options
+};
+void CheckClick (int x, int y)
+{
+	// See if anything was clicked, and if so, take the appropriate action
+};
+void DrawMenu ()
+{
+	SDL_SetRenderDrawColor(renderer,DropDownTabColor_R,DropDownTabColor_G,DropDownTabColor_B,SDL_ALPHA_OPAQUE);
+	// Draw all the tabs at the top.
+	SDL_SetRenderDrawColor(renderer,DropDownItemColor_R,DropDownItemColor_G,DropDownItemColor_B,SDL_ALPHA_OPAQUE);
+	// Draw all the dropped-down stuff.
 };
