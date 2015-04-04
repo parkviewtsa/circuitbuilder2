8c2239af7de9587f2272289081b265cd7c9a357f
rsethc <rsethc@gmail.com>
2015-04-04 11:02:09 -0400

    organizing screenshots for the Notebook
    all screenshots moved to "screenshots" subdirectory which itself may
    have subdirectories for multiple screenshots on one topic.

--- circuit-render-test.cpp
+++ circuit-render-test.cpp
@@ -3,4 +3,5 @@ int main ()
 {
 	// ".cbip" for "Circuit Builder Item Prototype"
 	if (crRequireProto("example-proto.cbip")) printf("crRequireProto failed with error code [%lu].\n",crGetError());
+	else printf("crRequireProto succeeded.\n");
 };
