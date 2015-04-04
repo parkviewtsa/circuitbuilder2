845611e1671a505078943d3d81fa9e061cbbd3ab
Brent Bessemer <bmb0610@gmail.com>
2015-04-04 11:20:37 -0400

    Revert "separated "circuit-render-proto.h" into its own file"
    This reverts commit 9252cd7cfdb17237f0edf174610c6867371d68e9.

--- circuit-render.h
+++ circuit-render.h
@@ -12,19 +12,67 @@ typedef unsigned int crIndex;
 #include <stdlib.h>
 #include <stdio.h>
 #include <circuit-render-error.h>
-#include <circuit-render-proto.h>
-struct crItem
+struct crLine
 {
-	crProto* proto;
-	crScalar posx,posy;
+	crScalar x1,y1,x2,y2;
 };
-/// The reason crItems is not a single-block list is
-/// so that expanding the list of items does not break
-/// the application's references to previously created items.
-crItem** crItems = NULL;
-crIndex crItemCount = 0;
-crItem* crCreateItem (char* path) // Returns NULL if something went wrong.
+struct crProto
 {
-	if (crRequireProto(path)) return; // crLastError will remain from the lower level.
-	crItem
+	crLine* lines;
+	crIndex linecount;
+	char* loadedfrom;
+};
+crProto** crProtos = NULL;
+crIndex crProtoCount = 0;
+bool crRequireProto (char* path) /// Returns presence of an error. (false: no error, true: error)
+{
+	if (!path)
+	{
+		crLastError = crError_NoProtoPath;
+		return true;
+	};
+	FILE* file = fopen(path,"r");
+	if (!file)
+	{
+		crLastError = crError_NoProtoFile;
+		return true;
+	};
+	crIndex linecount;
+	// Get number of lines.
+	if (!fscanf(file,"%lu",&linecount))
+	{
+		crLastError = crError_BadProtoFile;
+		return true;
+	};
+	// Get actual lines.
+	crLine* lines = malloc(sizeof(crLine) * linecount);
+	if (!lines)
+	{
+		// That memory was really important to me.
+		crLastError = crError_FailedAlloc;
+		return true;
+	};
+	crProto out;
+	out.linecount = linecount;
+	out.lines = lines;
+	out.loadedfrom = path;
+	for (unsigned long i; i < linecount; i++)
+	{
+		crScalar x1,y1,x2,y2;
+		if (fscanf(file,"%lf%lf%lf%lf",&x1,&y1,&x2,&y2) < 4)
+		{
+			crLastError = crError_BadProtoFile;
+			return true;
+		};
+		lines->x1 = x1;
+		lines->y1 = y1;
+		lines->x2 = x2;
+		lines->y2 = y2;
+		lines++;
+	};
+	fclose(file);
+	// Now add this to the archive.
+
+
+	return false;
 };
