4aa36d7bbf92f3c5c1e3db02cb29b9e33e79cd12
rsethc <rsethc@gmail.com>
2015-04-04 10:57:30 -0400

    started on renderer prototype loader
    extremely simplified form of the SVG concept.
    
    Error reporting system for the CircuitBuilder2 app to use.

---
+++ circuit-render-test.cpp
@@ -0,0 +1,6 @@
+#include <circuit-render.h>
+int main ()
+{
+	// ".cbip" for "Circuit Builder Item Prototype"
+	if (crRequireProto("example-proto.cbip")) printf("crRequireProto failed with error code [%lu].\n",crGetError());
+};
